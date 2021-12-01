0459bdd6a42d68003025bda730661f5bd2dd9391
Temur Esanov <temuresanov01@gmail.com>
2021-12-01 11:58:59 +0100

    Friend add

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,16 +1,17 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import s from './FriendListItem.module.css';
-import defaultImage from '../../images/defaultImage.jpg';
+import styles from './FriendListItem.module.css';
+import defaultImage from '../images/defaultImage.jpg';
 
 // const FriendListItem = ({ friend: { avatar = defaultImage, name, isOnline } }) => (
 const FriendListItem = ({ avatar = defaultImage, name, isOnline }) => (
-  <li className={s.item}>
+  <li className={styles.item}>
     <span
-      className={s.status}
+      className={styles.status}
       style={{ backgroundColor: isOnline ? 'green' : 'red' }}
     ></span>
-    <img className={s.avatar} src={avatar} alt={name} width="96px" />
-    <p className={s.name}>{name}</p>
+    <img className={styles.avatar} src={avatar} alt={name} width="46px" />
+    <p className={styles.name}>{name}</p>
   </li>
 );
 
